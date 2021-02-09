ddfb4893c8036e6fd56abc087f06d63bc7fd6369
muweiming <714990877@qq.com>
2021-02-09 13:38:02 +0800

    ycx3

--- src/main/java/io/buildpacks/example/sample/SampleApplication.java
+++ src/main/java/io/buildpacks/example/sample/SampleApplication.java
@@ -1,4 +1,4 @@
-package io.buildpacks.example.sample;
+﻿package io.buildpacks.example.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +8,7 @@ public class SampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
-		System.out.println("I just want to be happy ！");
+		System.out.println("I just want to be happy ！！");
 	}
 
 }
